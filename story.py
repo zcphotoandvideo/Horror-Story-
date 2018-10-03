f222f7eeefe97529f7dd15b45e8d445df2bf25c3
Shawn Chacko <shawncha.71180@redlandsschools.net>
2018-10-03 06:57:35 +0000

    final

---
+++ story.py
@@ -0,0 +1,103 @@
+import time 
+print("This story takes place in the abandoned town of Astroworld")
+time.sleep(3)
+print("In Astroworld there are many cruel and uninmaginable crimes that have taken place")
+time.sleep(3)
+print("Over the years there has been a report that over 250 crimes of missing teenagers that end up dead and have been found with broken arms and legs that have been bathed with their own blood from stab wounds")
+time.sleep(3)
+print("Are you interested in going through the cruel story of one of the survivors of ASTROWORLD??")
+time.sleep(3)
+x = input('Please enter Yes or No: ')
+
+
+if x == ("yes"):
+    print('Well then lets get started')
+else:
+    print('No? Well then I guess your to weak to go through a scare in you life goodbye')
+    for x in 'python':
+        if x == 'no':
+            break
+    print('Goodbye')
+    
+    
+
+print('Your friends decide to go walk in the haunted Black Woods of Asrtoworld?')
+time.sleep(2)
+
+x = input('Do you go with your friends Yes or no? ')
+if x == ("Yes"):
+    print('You will meet at the enterance of the woods at 2:00 A.M. in the morning ')
+else:
+    print('Your friends will never talk to you again for being a coward!')
+time.sleep(2)
+
+
+
+    
+print('Your walking in the woods and you get the feeling your being watched so yoou turn on a flashlight to look around but the batteries are dead!') 
+time.sleep(2)
+
+x = input('Do you Yes turn back or No keep going? ')
+if x == ("Yes"):
+    print('You and your friends keep walking and one of you steps on something which coincidentally happens to be a flashlight but its coverered in blood.')
+else:
+    print('You guys want to turn back but you see a figure in the shadows coming towards you and you take off running deeper into the woods.')
+time.sleep(2)
+    
+    
+    
+    
+    
+    
+print('You stop running after a while and see that theres nothing around you anymore. Now you know that your being followed and that leaving the woods is going to be difficult...')
+time.sleep(2)
+print('You have two options one you can take the risk of going back and getting attacked by whatever is out there or your second option you can continue going ino the woods to try to find shelter untill morning. ')
+time.sleep (2)
+print("do you yes turn back or no keep going")
+time.sleep(2)
+x = input("enter yes or no")
+if x == ("Yes"):
+    print('You  end up getting slaughtered by the horrifying monster Goodbye...')
+else:
+    print('You find an abandoned mansion and you and your friends go inside of it.')
+time.sleep(2)
+
+
+
+
+print('Your inside the mansion and all of the walls are scratched up and there is a horrible smell coming from deeper in the house.')
+time.sleep(2)
+print('you start to walk down the hallway and notice that the walls are covered in blood and have scratch marks on them and there are pieces of furniture scattered everywhere')
+time.sleep(2)
+print('You and your friends are extremely tired and hungry')
+time.sleep(2)
+print("Your friend comes across the kitchen and finds that theres a refrigerator full of food.")
+time.sleep(2)
+print("What do you do?")
+time.sleep(2)
+print("Let your friend eat the food thats in there or do you tell him not to... ")
+x=input("Enter Yes to let him eat the food thats in there or No to tell him not to")
+if x == ("Yes"):
+    print("Your friend swallows the first bite and instantly get dizzy and then you realize that the food was posioned and your friend dies after a couple of minutes")
+else:
+    print("You save your friends life after finding out there was a neddle hidden within the food.")
+time.sleep(2)
+
+print ("It's currently 4:00 AM and your friends want to sleep!")
+time.sleep(2)
+print("Theres enough beds for your friends and youself")
+time.sleep(2)
+print("But you see that the door handle is covered in blood and a sign outside the room that reads USE AT YOUR RISK!")
+time.sleep(2)
+print("So you warn your friends but their to tired and don't listen to you and they open the door")
+time.sleep(2)
+print('As they all lay on their own beds...')
+print("ALL OF A SUDDEN YOUR FRIEND STARTS SCREAMING THIER HEARTS OUT BECAUSE OF THE NEEDLES THAT JUST WERE CONNECTED TO THE BED AS A TRAP!")
+print("You have to make a difficult choice to either leave your friend here and let the scary creature kill him or to take him out carefully and have ahigher chance of being chased?")
+x =input('Enter Yes to leave your friend or No to take him out and carry him')
+if x == ("Yes"):
+    print ('You escape but your friends stay back to care of the injured friend')
+else:
+    print("Saving the injured friend was a mistake by leading the creature right to you by the trail of blood!!")
+
+print("Saving your frinds gave the creature an easy way tofind and kill you")
